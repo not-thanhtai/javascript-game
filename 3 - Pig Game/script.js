61881383e1d4c2bcd2d7b8fdfa5a4536a4f46090
Thanh Tai Nguyen <tai606760@gmail.com>
2024-01-25 14:53:20 +0700

    feat: Update game logic

---
+++ 3 - Pig Game/script.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const player0El = document.querySelector('.player--0');
+const score0El = document.getElementById('score--0');
+const current0El = document.getElementById('current--0');
+
+const player1El = document.querySelector('.player--1');
+const score1El = document.getElementById('score--1');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+var scores, currentScore, activePlayer, state;
+
+const initializeGame = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  state = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+  player1El.classList.remove('player--winner');
+};
+initializeGame();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
+
+const handleNewGame = function () {
+  initializeGame();
+};
+
+const handleRollDice = function () {
+  if (state) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+};
+
+const handleHold = function () {
+  if (state) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      state = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+};
+
+btnNewGame.addEventListener('click', handleNewGame);
+
+btnRollDice.addEventListener('click', handleRollDice);
+
+btnHold.addEventListener('click', handleHold);
